3d793ebb26599e00438e8177d78eb890802027a7
Vishal Saini <vs456953@gmail.com>
2025-01-30 09:03:51 +0530

    all

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, { Application, Request, Response } from 'express';
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
+import { prismaClient } from './lib/db';
 
 async function init() {
   const app: Application = express();
@@ -15,12 +16,31 @@ async function init() {
         hello: String
         say(name: String): String
       }
+      type Mutation {
+        createUser( firstName : String! , lastName : String! ,email : String!, password : String!): Boolean
+      }
     `,
     resolvers: {
       Query: {
         hello: () => 'Hello World',
         say: (_, { name}) => `Hello ${name}`,
       },
+      Mutation :{
+        createUser : async (__,
+           {firstName , lastName ,email , password} : 
+           {firstName: string; lastName : string; email :string ; password : string;}) =>{
+                await prismaClient.user.create({
+                 data: {
+                  email,
+                  firstName,
+                  lastName,
+                  password,
+                  salt: 'random_salt',
+                 },
+                });
+                return true;
+        }
+      }
     },
   });
 
